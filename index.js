b4d9f2b37ecc260a0c4bc0af7d37ebafee8c301a
Tom Kopp <witzbould@live.com>
2017-11-24 18:54:08 +0100

    update port handler comment

--- index.js
+++ index.js
@@ -5,24 +5,17 @@ const SerialPort = require('serialport')
 const baudRate = 921600
 
 /**
- * @var ports
+ * @param {object} port desc
  * @example
- * [ { comName: 'COM13',
+ * { comName: 'COM13',
  *   manufacturer: 'Silicon Labs',
  *   serialNumber: '00BCAC79',
  *   pnpId: 'USB\\VID_10C4&PID_EA60\\00BCAC79',
  *   locationId: 'Port_#0007.Hub_#0006',
  *   vendorId: '10C4',
- *   productId: 'EA60' },
- * { comName: 'COM15',
- *   manufacturer: 'Silicon Labs',
- *   serialNumber: '00BCB26D',
- *   pnpId: 'USB\\VID_10C4&PID_EA60\\00BCB26D',
- *   locationId: 'Port_#0004.Hub_#0006',
- *   vendorId: '10C4',
- *   productId: 'EA60' } ]
+ *   productId: 'EA60' }
+ * @returns {ChildProcess} handle
  */
-
 const spawnPortHandler = (port) => spawn('node serialPort.js', [ port.comName, port.serialNumber, baudRate ], { detached: true, shell: true })
 
 
@@ -34,4 +27,4 @@ SerialPort
 		spawn.stderr.on('data', (data) => process.stderr.write(`\nstderr: ${data}`))
 		spawn.on('close', (code) => process.stderr.write(`\nchild process exited with code ${code}\n`))
 	}))
-	.catch((reason) => process.stdout.write(reason.message))
+	.catch((err) => process.stdout.write(err.message))
