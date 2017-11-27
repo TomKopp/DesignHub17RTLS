ea5a6cf7717c83b72a16e2d7baa8e3a5559d2b4a
Tom Kopp <witzbould@live.com>
2017-11-27 23:01:49 +0100

    working lateration

--- src/lateration.js
+++ src/lateration.js
@@ -1,18 +1,13 @@
 const { readFile } = require('fs')
 const { promisify, debuglog, inspect } = require('util')
+const readline = require('readline')
 const trilat = require('trilat')
 
-/*
-[
-//      X     Y     R
-    [ 0.0,  0.0, 10.0],
-    [10.0, 10.0, 10.0],
-    [10.0,  0.0, 14.142135]
-]
- */
-const [ ,, positions ] = process.argv
+
+const [ ,, positionsConfigURI ] = process.argv
 const readFileAsync = promisify(readFile)
 const debugLogger = debuglog('debug')
+const rl = readline.createInterface({ input: process.stdin })
 
 
 /**
@@ -26,6 +21,11 @@ const debugLoggerStderr = (data) => {
 	return data
 }
 
+/**
+ * Log messages to stderr
+ * @param	{any}	data any data will be parsed with util.inspect
+ * @returns	{void}
+ */
 const loggerStderr = (data) => {
 	process.stderr.write(inspect(data))
 
@@ -35,18 +35,78 @@ const loggerStderr = (data) => {
 /**
  * http://2ality.com/2015/08/es6-map-json.html
  * @param	{string}	jsonStr		string representation of multidimensional array
- * @returns	{Map}					Map with ranges
+ * @returns	{Promise}				resolves with Map containing ranges
  */
-const jsonToMap = (jsonStr) => new Map(JSON.parse(jsonStr))
+const jsonToMap = (jsonStr) => new Promise((resolve, reject) => {
+	try {
+		resolve(new Map(JSON.parse(jsonStr)))
+	} catch (err) {
+		reject(err)
+	}
+})
 
+/**
+ * [description]
+ * @param	{Map}	positions	[description]
+ * @param	{Map}	ranges		[description]
+ * @returns	{Map}				[description]
+ */
 const mergePositionsRanges = (positions, ranges) => {
+	const ret = new Map()
 
+	ranges.forEach((dist, src) => {
+		if (positions.has(src)) {
+			const { x, y } = positions.get(src)
+
+			ret.set(src, [ x, y, dist ])
+		}
+	})
+
+	return ret
 }
 
+/**
+ * does effectively the same as a reduce
+ * @param	{Map}	data	[description]
+ * @returns	{Array}			[description]
+ */
+const mapToTrilatInput = (data) => {
+	const ret = []
 
-readFileAsync(positions, 'utf8')
+	data.forEach((val) => {
+		ret.push(val)
+	})
+
+	return ret
+}
+
+/**
+ * [description]
+ * @param	{func}		mergeFn	[description]
+ * @param	{string}	line	[description]
+ * @returns	{Promise}			[description]
+ */
+const rangesPositionsToCoords = (mergeFn, line) => jsonToMap(line)
+	.then(mergeFn)
 	.then(debugLoggerStderr)
+	.then(mapToTrilatInput)
+	.then(trilat)
+	.catch((err) => {
+		loggerStderr(err)
+
+		return [ null, null ]
+	})
+
+
+readFileAsync(positionsConfigURI, 'utf8')
 	.then(jsonToMap)
-	.then(debugLoggerStderr)
-	// .then(/*attach stdin listener*/)
+	.then((positions) => (ranges) => mergePositionsRanges(positions, ranges))
+	.then((fn) => (line) => rangesPositionsToCoords(fn, line))
+	.then((fn) => {
+		rl.on('line', (line) => {
+			fn(line)
+				// write to stdout
+				.then(loggerStderr)
+		})
+	})
 	.catch(loggerStderr)
